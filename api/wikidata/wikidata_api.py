cbf77038f8713ef75405817dd0d2ffa7946760a7
GARZTHONGK <albert.bruun.thomsen@gmail.com>
2024-04-22 10:13:05 +0200

    Add documentation for existing code

--- api/wikidata/wikidata_api.py
+++ api/wikidata/wikidata_api.py
@@ -4,14 +4,28 @@ from database.data import Station
 from datetime import datetime
 
 
-
-
-
 def parse_date(date: str):
+    """
+    This function takes a date string as input and returns a datetime.date object.
+    The input date string should be in the format "%Y-%m-%dT%H:%M:%SZ".
+
+    Args:
+        date (str): The date string to be parsed.
+
+    Returns:
+        datetime.date: The parsed date.
+    """
     return datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ").date()
 
 
 def get_results():
+    """
+    This function sends a SPARQL query to the global endpoint URL and returns the results in JSON format.
+    The endpoint URL and the query are defined globally.
+
+    Returns:
+        dict: The results of the query in JSON format.
+    """
     global endpoint_url
     global query
     user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
@@ -22,15 +36,22 @@ def get_results():
 
 
 def parse_wikidata(results):
+    """
+    This function takes the results from a SPARQL query to Wikidata and parses the data into a list of Station objects.
+
+    Args:
+        results (dict): The results of the SPARQL query in JSON format.
+
+    Returns:
+        list: A list of Station objects, each representing a station with its associated data.
+    """
     station_data = []
     for result in results["results"]["bindings"]:
         data_dict = {
-
             "station": result["station"]["value"],
             "name": result["stationLabel"]["value"],
             "uic_code": result["stationID"]["value"] if "stationID" in result else None,
-            "transport_network": result["transportNetworkLabel"][
-                "value"] if "transportNetworkLabel" in result else None,
+            "transport_network": result["transportNetworkLabel"]["value"] if "transportNetworkLabel" in result else None,
             "address": result["address"]["value"] if "address" in result else None,
             "type": result["BaneTypeLabel"]["value"] if "BaneTypeLabel" in result else None,
             "connecting_line": result["connectingLineLabel"]["value"] if "connectingLineLabel" in result else None,
